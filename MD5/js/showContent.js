f75eef886c8a28e5fcc7b738019a6c90c4e86835
Dongsheng Fang <451061754@qq.com>
2016-10-27 16:43:42 +0800

    md5

---
+++ MD5/js/showContent.js
@@ -0,0 +1,36 @@
+$(document).ready(function(){
+
+    $("#login").click(function(){
+        var userName = $("#username").val();
+        var passWord = $("#password").val();
+        $("#showUsername").text(userName);
+        $("#showUsernameMD5").text(md5(userName));
+        $("#showPassword").text(passWord);
+        $("#showPasswordMD5").text(md5(passWord));
+
+        if("3be043014dec4bca29c01d8802ed292d" == md5(userName) &&
+            "ca526eec71b66549f2b01a23ec0b8ece" == md5(passWord)){
+            getDecryptResume(passWord);
+        } else if("8f43d75cc76f8d6ea49b67e618e678a0" == md5(userName) &&
+            "158ec274fc60981bde7ad54cf7d44f76" == md5(passWord)){
+            getDecryptResume(passWord);
+        }
+    });
+});
+
+function getDecryptResume(password) {
+    $.getJSON("js/" + md5(md5(password)).substr(0, 8) + ".json", function(data){
+        var $jsontip = $("#content");
+        var resumeContent;
+        $jsontip.empty();
+        $.each(data,function(infoIndex, info){  
+            resumeContent = info["content"];
+        })
+        resumeContent = resumeContent.replace(/@/g, password.substr(1, 1));
+        resumeContent = resumeContent.replace(/\$/g, password.substr(3, 1));
+        resumeContent = resumeContent.replace(/#/g, password.substr(5, 1));
+        var decryptResume = unescape(resumeContent);
+        $("#main").fadeOut();
+        $jsontip.html(decryptResume);
+    })
+}
