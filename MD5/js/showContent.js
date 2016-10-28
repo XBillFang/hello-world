4ffbc690015ebfb856cb2365332ae7a2940b5cde
Dongsheng Fang <451061754@qq.com>
2016-10-28 15:41:11 +0800

    MD5 加密更新

--- MD5/js/showContent.js
+++ MD5/js/showContent.js
@@ -1,6 +1,6 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
-    $("#login").click(function(){
+    $("#login").click(function() {
         var userName = $("#username").val();
         var passWord = $("#password").val();
         $("#showUsername").text(userName);
@@ -8,22 +8,23 @@ $(document).ready(function(){
         $("#showPassword").text(passWord);
         $("#showPasswordMD5").text(md5(passWord));
 
-        if("3be043014dec4bca29c01d8802ed292d" == md5(userName) &&
-            "ca526eec71b66549f2b01a23ec0b8ece" == md5(passWord)){
-            getDecryptResume(passWord);
-        } else if("8f43d75cc76f8d6ea49b67e618e678a0" == md5(userName) &&
-            "158ec274fc60981bde7ad54cf7d44f76" == md5(passWord)){
-            getDecryptResume(passWord);
+        if ("3be043014dec4bca29c01d8802ed292d" == md5(userName) &&
+            "ca526eec71b66549f2b01a23ec0b8ece" == md5(passWord)) {
+            getDecryptResume(userName, passWord);
+        } else if ("8f43d75cc76f8d6ea49b67e618e678a0" == md5(userName) &&
+            "158ec274fc60981bde7ad54cf7d44f76" == md5(passWord)) {
+            getDecryptResume(userName, passWord);
         }
     });
 });
 
-function getDecryptResume(password) {
-    $.getJSON("js/" + md5(md5(password)).substr(0, 8) + ".json", function(data){
+function getDecryptResume(username, password) {
+    var folderName = md5(username + password).substr(0, 8);
+    $.getJSON("resource/" + folderName + "/json/resume.json", function(data) {
         var $jsontip = $("#content");
         var resumeContent;
         $jsontip.empty();
-        $.each(data,function(infoIndex, info){  
+        $.each(data, function(infoIndex, info) {
             resumeContent = info["content"];
         })
         resumeContent = resumeContent.replace(/@/g, password.substr(1, 1));
